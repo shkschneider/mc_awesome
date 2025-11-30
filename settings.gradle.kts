4614bdbd25ca20e9baf416d8e879db6314ce9b4b
ShkSchneider <shkschneider@noreply.codeberg.org>
2025-11-30 10:56:14 +0100

    cleanup

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,9 +1,5 @@
 rootProject.name = "Awesome"
 
-// Unified mod - all modules merged into single project
-// Machines module can be optionally included if needed
-// include(":machines")
-
 pluginManagement {
     repositories {
         maven(url = "https://plugins.gradle.org/m2") { name = "gradle" }
